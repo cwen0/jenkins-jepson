21cb56faa2960bef873debbe3df7ab6f7dec972c
cwen0 <cwenyin0@gmail.com>
2017-08-08 19:23:53 +0800

    update

--- jepsen_test.groovy
+++ jepsen_test.groovy
@@ -12,15 +12,23 @@ def call(JEPSEN_BRANCH) {
                    // checkout scm
                    git credentialsId: 'github-iamxy-ssh', url: "$BUILD_URL", branch: "${JEPSEN_BRANCH}"
                    githash = sh(returnStdout: true, script: "git rev-parse HEAD").trim()
-                   sh "cd docker&&bash up.sh"
                }
             }
             stage('test') {
-                echo "success"
+               dir("${ws}/jepsen") {
+                    def result = sh(script: "docker exec jepsen-control bash -c 'cd /jepsen/tidb && ./run.sh'", returnStdout: true)
+               }
             }
-            stage('clean') {
-                sh "ls -l"
-                sh "rm -rf jepsen"
+             stage('Summary') {
+                def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+                def slackmsg = "[${env.JOB_NAME.replaceAll('%2F','/')}-${env.BUILD_NUMBER}] `${result}`" + "\n" +
+                "Elapsed Time: `${duration}` Mins" + "\n""
+
+                if (result != "SUCCESS") {
+                    slackSend channel: '#octopus', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+                } else {
+                    slackSend channel: '#octopus', color: 'good', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+                }
             }
         }
     }
