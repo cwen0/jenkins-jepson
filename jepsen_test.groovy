2178f23e72b3c74fdfb2cc0d65ac6927360f381c
cwen0 <cwenyin0@gmail.com>
2017-08-08 11:02:12 +0800

    init jepsen test

--- jepsen_test.groovy
+++ jepsen_test.groovy
@@ -12,8 +12,9 @@ def call(JEPSEN_BRANCH) {
                    // checkout scm
                    git credentialsId: 'github-iamxy-ssh', url: "$BUILD_URL", branch: "${JEPSEN_BRANCH}"
                    githash = sh(returnStdout: true, script: "git rev-parse HEAD").trim()
-
-                   sh "cd jepsen/docker&&bash run.sh"
+                   sh "ls -l"
+                   sh "cd docker&& ls -l"
+                   sh "bash run.sh"
                }
             }
             stage('test') {
