661b92b312663ba422cf1a32dfe127baf7a73fa7
cwen0 <cwenyin0@gmail.com>
2017-08-09 14:12:38 +0800

    update

--- jepsen_test.groovy
+++ jepsen_test.groovy
@@ -1,5 +1,5 @@
-def call(RELEASE_URL, JEPSEN_BRANCH) {
-    def BUILD_URL = 'git@github.com:UncP/jepsen.git'
+def call(RELEASE_URL, JEPSEN_BRANCH, TIDB_BRANCH, TIKV_BRANCH, PD_BRANCH) {
+    def BUILD_URL = 'git@github.com:pingcap/jepsen.git'
     def UCLOUD_OSS_URL = "http://pingcap-dev.hk.ufileos.com"
     env.PATH = "/data/jenkins/bin:/bin:${env.PATH}"
     def tidb_sha1, tikv_sha1, pd_sha1
@@ -16,22 +16,25 @@ def call(RELEASE_URL, JEPSEN_BRANCH) {
                sh "docker cp ./jepsen jepsen-control:/jepsen"
             }
             stage('test') {
-                    sh "echo 'start'"
                     sh "docker exec jepsen-control bash -c 'cd /jepsen/tidb/ && ./run.sh ${RELEASE_URL}'"
-                    //sh "echo ${result}"
-                    result = "SUCCESS"
-            }
-            stage('symmary') {
-                def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
-                def slackmsg = "[${env.JOB_NAME.replaceAll('%2F','/')}-${env.BUILD_NUMBER}] `${result}`" + "\n" + "Elapsed Time: `${duration}` Mins" + "\n"
-                if (result != "SUCCESS") {
-                    slackSend channel: '#octopus', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
-                } else {
-                    slackSend channel: '#octopus', color: 'good', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
-                }
             }
         }
+        result = "SUCCESS"
     }
+   stage('symmary') {
+       def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+       def slackmsg = "[${env.JOB_NAME.replaceAll('%2F','/')}-${env.BUILD_NUMBER}] `${result}`" + "\n" +
+       "Elapsed Time: `${duration}` Mins" + "\n"  +
+       "tidb Branch: `${TIDB_BRANCH}` + "\n" +
+       "tikv Branch: `${TIKV_BRANCH}` + "\n" +
+       "pd   Branch: `${PD_BRANCH}` + "\n"
+
+       if (result != "SUCCESS") {
+           slackSend channel: '#octopus', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+       } else {
+           slackSend channel: '#octopus', color: 'good', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+       }
+   }
 }
 
 return this
